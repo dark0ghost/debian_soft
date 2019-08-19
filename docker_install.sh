5f28cd6fed6847de72fded9a9dd738a5eb71c853
dark0ghost <40036029+dark0ghost@users.noreply.github.com>
2019-08-19 20:50:50 +0300

    Update docker_install.sh

--- docker_install.sh
+++ docker_install.sh
@@ -17,7 +17,7 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io
 echo "test "
 grep docker /etc/group
 sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m) -o/usr/local/bin/docker-compose
-sudo chmod +x/usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s/usr/local/bin/docker-compose/usr/bin/docker-compose
 sudo usermod -aG docker $(whoami)
 sudo docker run hello-world
