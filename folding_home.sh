7358ea5625564b40747f5f99a731472aed5be802
dark0ghost <40036029+dark0ghost@users.noreply.github.com>
2020-03-16 15:49:18 +0300

    Create folding_home.sh

---
+++ folding_home.sh
@@ -0,0 +1,7 @@
+
+wget https://download.foldingathome.org/releases/public/release/fahclient/debian-testing-64bit/v7.4/fahclient_7.4.4_amd64.deb
+wget https://download.foldingathome.org/releases/public/release/fahcontrol/debian-testing-64bit/v7.4/fahcontrol_7.4.4-1_all.deb
+wget https://download.foldingathome.org/releases/public/release/fahviewer/debian-testing-64bit/v7.4/fahviewer_7.4.4_amd64.deb
+sudo dpkg -i --force-depends fahclient_7.4.4_amd64.deb
+sudo dpkg -i --force-depends fahcontrol_7.4.4-1_all.deb
+sudo dpkg -i --force-depends fahviewer_7.4.4_amd64.deb
