55fb032cde36f751cb6e896b3824ccd28435405d
dark0ghost <40036029+dark0ghost@users.noreply.github.com>
2020-08-13 22:43:58 +0300

    Update steam_intall.sh

--- steam_intall.sh
+++ steam_intall.sh
@@ -1,8 +1,8 @@
-#wrtite in /etc/apt/sources.list
-#deb http://deb.debian.org/debian/ buster main non-free contrib
-#deb-src http://deb.debian.org/debian/ buster main non-free contrib
-#deb http://security.debian.org/debian-security buster/updates main contrib non-free
-#deb-src http://security.debian.org/debian-security buster/updates main contrib non-free
+#!/usr/bin
+sudo add-apt-repository "deb http://deb.debian.org/debian/ buster main non-free contrib"
+sudo add-apt-repository "deb-src http://deb.debian.org/debian/ buster main non-free contrib"
+sudo add-apt-repository "deb http://security.debian.org/debian-security buster/updates main contrib non-free"
+sudo add-apt-repository "deb-src http://security.debian.org/debian-security buster/updates main contrib non-free"
 sudo dpkg --add-architecture i386
 sudo apt update
 sudo apt install steam -y
