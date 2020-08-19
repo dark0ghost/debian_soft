ea07c57e59f52f863596bef2b538d521dc88222e
dark0ghost <40036029+dark0ghost@users.noreply.github.com>
2020-08-19 22:54:17 +0300

    Update docker_install.sh

--- docker_install.sh
+++ docker_install.sh
@@ -21,5 +21,5 @@ sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-c
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose/usr/bin/docker-compose
 sudo usermod -aG docker $(whoami)
-sudo docker run hello-world
+docker run hello-world
 
