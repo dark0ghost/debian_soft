804f502fc29589ca0905290111d0ea793bce2e02
dark0ghost <40036029+dark0ghost@users.noreply.github.com>
2020-08-19 22:56:55 +0300

    Update docker_install.sh

--- docker_install.sh
+++ docker_install.sh
@@ -17,7 +17,7 @@ sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 echo "test "
 grep docker /etc/group
-sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m) -o/usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m) -o/usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose/usr/bin/docker-compose
 sudo usermod -aG docker $(whoami)
