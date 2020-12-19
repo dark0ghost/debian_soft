2c344256388c5895b0300aee33bd629f5cdf64f1
dark0ghost <40036029+dark0ghost@users.noreply.github.com>
2020-12-19 23:09:22 +0300

    Update docker_install.sh

--- docker_install.sh
+++ docker_install.sh
@@ -1,25 +1,8 @@
 #!/usr/bin
-sudo apt-get remove docker docker-engine docker.io containerd runc
-sudo apt-get update
-sudo apt-get install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg2 \
-    software-properties-common -y
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/debian \
-   $(lsb_release -cs) \
-   stable"
+echo 'deb [arch=amd64] https://download.docker.com/linux/debian buster stable' | sudo tee /etc/apt/sources.list.d/docker.list
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io -y
-echo "test "
-grep docker /etc/group
-sudo curl -L https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m) -o/usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-sudo ln -s /usr/local/bin/docker-compose/usr/bin/docker-compose
-sudo usermod -aG docker $(whoami)
+sudo apt-get remove docker docker-engine docker.io -y
+sudo apt-get install docker-ce -y
+sudo usermod -aG docker $USER
 docker run hello-world
-
