ead683bad2b92e74457f590e890829d697382a55
dark0ghost <40036029+dark0ghost@users.noreply.github.com>
2022-01-03 23:47:20 +0300

    Update tp-wn725n.sh
    use all core for compile

--- tp-wn725n.sh
+++ tp-wn725n.sh
@@ -6,7 +6,7 @@ sudo apt-get install build-essential
 sudo apt-get install git
 git clone https://github.com/lwfinger/rtl8188eu.git
 cd rtl8188eu
-make all
+make all -j $(cat /proc/cpuinfo | grep 'cpu cores' | head -1 | cut -d' ' -f3)
 sudo make install 
 sudo insmod 8188eu.ko 
 zcat /proc/config.gz | grep CONFIG_ATH
